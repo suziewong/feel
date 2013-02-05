5f41bd1c000159688f98e4d880698f7db79a77cd
suziewong <monkeysuzie@gmail.com>
2013-02-05 18:32:33 +0800

    config

--- Conf/config.php
+++ Conf/config.php
@@ -6,7 +6,7 @@ return array(
 	'DB_HOST'   => 'localhost', // 服务器地址
 	'DB_NAME'   => 'feel', 		// 数据库名
 	'DB_USER'   => 'suzie', 	// 用户名
-	'DB_PWD'    => '027267', 	// 密码
+	'DB_PWD'    => '', 	// 密码
 	'DB_PORT'   => 3306, 		// 端口
 	'DB_PREFIX' => 'feel_', 	// 数据库表前缀
 
