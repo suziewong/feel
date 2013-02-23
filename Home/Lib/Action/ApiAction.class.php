29555bd8e990b8eb99c61383ab5caff36a0ff0c4
suziewong <monkeysuzie@gmail.com>
2013-02-23 13:21:32 +0800

    api

--- Home/Lib/Action/ApiAction.class.php
+++ Home/Lib/Action/ApiAction.class.php
@@ -35,9 +35,37 @@ class ApiAction extends Action {
 	public function content()
 	{
 		$Content = M("Content");
-		$classid = isset($_GET['classid'])?$_GET['classid']:2;
-		$condition['classid']=$classid;
-		$Content = $Content->where($condition)->select();
+		$contentid = isset($_GET['contentid'])?$_GET['contentid']:"";
+		$condition['contentid']=$contentid;
+		if($condition['contentid'] == "")
+		{
+			$classid = isset($_GET['classid'])?$_GET['classid']:2;
+			$cond['classid']=$classid;
+			$Content = $Content->where($cond)->select();
+		}
+		else
+		{
+			$Content = $Content->where($condition)->select();
+		}
+
+		
 		echo json_encode($Content);
-	}	
+	}
+	//主播生活照
+	public function get_life()
+	{
+		$Life = M("Life");
+		$djid = isset($_GET['djid'])?$_GET['djid']:"";
+		$condition['djid']=$djid;
+
+		if($condition['djid'] == "")
+		{
+			$Life = $Life->select();
+		}
+		else
+		{
+			$Life = $Life->where($condition)->select();
+		}
+		echo json_encode($Life);
+	}
 }
