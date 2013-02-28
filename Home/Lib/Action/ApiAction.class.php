f1f8eb72a60ed6ea8eeb200dee8eeeee62d720e4
suzie <suzie@testServer89.(none)>
2013-02-28 16:16:53 +0800

    change apis

--- Home/Lib/Action/ApiAction.class.php
+++ Home/Lib/Action/ApiAction.class.php
@@ -23,10 +23,48 @@ class ApiAction extends Action {
 		if($condition['id'] == "")
 		{
 			$Dj = $Dj->select();
+			for($i=0;$i<count($Dj);$i++)
+			{
+				
+				$cond['djid'] = $Dj[$i]['id'];
+				$Life = M("Life");
+				$Life = $Life->where($cond)->select();
+				for($j=0; $j<count($Life);$j++)
+				{
+					$Dj[$i]['lifePics'][$j] = $Life[$j]['lifeaddress'];
+				}
+				$Dj[$i]['headPic']= $Dj[$i]['headaddress'];
+				$Dj[$i]['facePic']= $Dj[$i]['faceaddress'];
+				$Dj[$i]['column']= $Dj[$i]['total'];
+				$Dj[$i]['weibo']= $Dj[$i]['weibo-name'];
+				$Dj[$i]['weiboUrl']= $Dj[$i]['weibo-url'];
+				unset($Dj[$i]['headaddress']);
+				unset($Dj[$i]['faceaddress']);
+				unset($Dj[$i]['total']);
+				unset($Dj[$i]['weibo-name']);
+				unset($Dj[$i]['weibo-url']);
+			}
 		}
 		else
 		{
 			$Dj = $Dj->where($condition)->select();
+			$cond['djid'] = $condition['id'];
+			$Life = M("Life");
+			$Life = $Life->where($cond)->select();
+			for($i=0; $i<count($Life);$i++)
+			{
+				$Dj[0]['lifePics'][$i] = $Life[$i]['lifeaddress'];
+			}
+			$Dj[0]['headPic']= $Dj[0]['headaddress'];
+			$Dj[0]['facePic']= $Dj[0]['faceaddress'];
+			$Dj[0]['column']= $Dj[0]['total'];
+			$Dj[0]['weibo']= $Dj[0]['weibo-name'];
+			$Dj[0]['weiboUrl']= $Dj[0]['weibo-url'];
+			unset($Dj[0]['headaddress']);
+			unset($Dj[0]['faceaddress']);
+			unset($Dj[0]['total']);
+			unset($Dj[0]['weibo-name']);
+			unset($Dj[0]['weibo-url']);
 		}
 		echo json_encode($Dj);
 
@@ -42,10 +80,33 @@ class ApiAction extends Action {
 			$classid = isset($_GET['classid'])?$_GET['classid']:2;
 			$cond['classid']=$classid;
 			$Content = $Content->where($cond)->select();
+			for($i=0; $i<count($Content);$i++)
+			{
+						
+				$Content[$i]['title'] = $Content[$i]['contentname'];
+				$Content[$i]['file'] = $Content[$i]['mp3address'];
+				$member = $Content[$i]['member'];
+				unset($Content[$i]['contentname']);
+				unset($Content[$i]['mp3address']);
+				unset($Content[$i]['number']);
+				unset($Content[$i]['member']);
+				$Content[$i]['member'][0] = $member;	
+			}
+
+			
+			
 		}
 		else
 		{
 			$Content = $Content->where($condition)->select();
+			$Content[0]['title'] = $Content[0]['contentname'];
+			$Content[0]['file'] = $Content[0]['mp3address'];
+			$member = $Content[0]['member'];
+			unset($Content[0]['contentname']);
+			unset($Content[0]['mp3address']);
+			unset($Content[0]['number']);
+			unset($Content[0]['member']);
+			$Content[0]['member'][0] = $member;	
 		}
 
 		
